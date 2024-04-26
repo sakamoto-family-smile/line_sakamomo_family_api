57278271a8f8eddbce1a1d6d0195581efd17566c
sakamoto-family-smile <sakamoto-family-smile@gmail.com>
2024-04-27 00:39:50 +0900

    implement

--- app/agent.py
+++ app/agent.py
@@ -4,9 +4,12 @@ import requests
 from logging import Logger
 
 from langchain.agents import AgentType, initialize_agent, load_tools
-from langchain_openai import OpenAI
-from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_vertexai import VertexAI
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain.memory import ConversationBufferMemory
+from langchain_google_firestore import FirestoreChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+import uuid
 
 
 class WeatherInfo(BaseModel):
@@ -19,6 +22,11 @@ class WeatherInfo(BaseModel):
 
 class LLMAgentResponse(BaseModel):
     text: str
+
+
+# TODO : imp
+class CustomAgentConfig(BaseModel):
+    pass
 
 
 class AgentUtil:
@@ -56,9 +64,10 @@ class CustomAgent:
     def __init__(self, logger: Logger) -> None:
         self.__logger = logger
 
-        # OpenAI Agentの作成
+        # LLM Agentの作成
         #llm = OpenAI(temperature=0)
         #llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-preview-0409", temperature=0.9) # gemini-pro
+        # TODO : メモリ機能を追加する
         llm = VertexAI(
             model_name="gemini-1.5-pro-preview-0409",
             temperature=0.5,
@@ -68,8 +77,28 @@ class CustomAgent:
         )
         os.environ["OPENWEATHERMAP_API_KEY"] = os.environ["OPEN_WEATHER_KEY"]
         tools = load_tools(["openweathermap-api"], llm)
-        self.__openai_agent = initialize_agent(
+        self.__agent = initialize_agent(
             tools=tools, llm=llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+        )
+        memory = self.get_chat_message_history(
+            memory_type="firestore",
+            config={
+                "session_id": "session_id", "collection": "HistoryMessages"
+            }
+        )
+
+        # debug
+        messages = memory.messages
+        for message in messages:
+            self.__logger.info(message)
+
+        self.__agent_with_chat_history = RunnableWithMessageHistory(
+            self.__agent,
+            # This is needed because in most real world scenarios, a session id is needed
+            # It isn't really used here because we are using a simple in memory ChatMessageHistory
+            lambda session_id: memory,
+            input_messages_key="input",
+            history_messages_key="chat_history",
         )
 
     def get_weather_info(self, area_name: str) -> WeatherInfo:
@@ -77,5 +106,26 @@ class CustomAgent:
         return info
 
     def get_llm_agent_response(self, text: str) -> LLMAgentResponse:
-        res = self.__openai_agent.run(text)
-        return LLMAgentResponse(text=res)
+        self.__logger.info("start get_llm_agent_response...")
+        #res = self.__agent.run(text)
+        #session_id = str(uuid.uuid4())
+        res = self.__agent_with_chat_history.invoke(
+            {
+                "input": text
+            },
+            config = {
+                "configurable": {"session_id": "session_id"}
+            })
+        return LLMAgentResponse(text=res["output"])
+
+    def get_chat_message_history(self, memory_type: str, config: dict) -> BaseChatMessageHistory:
+        if memory_type == "local":
+            chat_buffer = ConversationBufferMemory()
+            return chat_buffer.chat_memory
+        elif memory_type == "firestore":
+            return FirestoreChatMessageHistory(
+                session_id=config["session_id"],
+                collection=config["collection"]
+            )
+        else:
+            raise NotImplementedError(f"{memory_type} memory type is not implemented!")
