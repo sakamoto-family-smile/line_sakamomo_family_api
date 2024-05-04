524dc8eeda4b9e774e4a8b875c036e88ab184110
sakamoto-family-smile <sakamoto-family-smile@gmail.com>
2024-05-04 18:55:37 +0900

    implement the todo function

--- app/agent_util.py
+++ app/agent_util.py
@@ -1,11 +1,18 @@
+from pydantic import BaseModel
 from datetime import datetime
 from logging import Logger, getLogger, StreamHandler
+from typing import List
 from .firebase_util import get_db_client_with_default_credentials
 
 
 local_logger = getLogger(__name__)
 local_logger.addHandler(StreamHandler())
 local_logger.setLevel("DEBUG")
+
+
+class TodoData(BaseModel):
+    date: datetime
+    content: str
 
 
 class TodoHandler:
@@ -25,7 +32,7 @@ class TodoHandler:
             raise TodoHandleError(f"input text format is invalid! text is {input_text}")
 
         if datas[0] != "TODO":
-            raise TodoHandleError(f"input text format is invalid! text is {input_text}")
+            raise TodoHandleError(f"input text format is invalid! head text data is not 'TODO'. text is {input_text}")
 
         # TODOに関する処理を実施
         if len(datas) == 2:
@@ -36,6 +43,10 @@ class TodoHandler:
             # TODOの登録処理
             target_date = datetime.strptime(datas[1], "%Y%m%d")
             content = datas[2]
+            self.register_todo_from_text(
+                target_date=target_date,
+                content=content
+            )
 
     def register_todo_from_text(self, target_date: datetime, content: str):
         """文字列からTODOの内容と日付情報を取得する.
@@ -53,7 +64,7 @@ class TodoHandler:
 
         # 日付情報とTODO情報をデータベースに登録
         data = {
-            "date": target_date, "todo": content
+            "date": target_date, "content": content
         }
         try:
             self.db.collection(self.collection_id) \
@@ -64,16 +75,29 @@ class TodoHandler:
                 f"inserting data into db is error! error detail is {e}"
             )
 
-    def get_todo_list_from_text(self, target_date: datetime):
+    def get_todo_list_from_text(self, target_date: datetime) -> List[TodoData]:
         self.logger.info(
             f"start to get the todo list. target_date is {target_date}"
         )
 
-        # 文字列から日付情報を抽出
-
-
         # 日付情報から、TODO一覧を取得
-        pass
+        # TODO : 取得する際に日付でフィルタをするようにしたい。そして、かなり遅い取り方になっている
+        try:
+            documents = self.db.collection(self.collection_id) \
+                               .document(self.document_id) \
+                               .collections()
+            res = []
+            for document in documents:
+                d = document.to_dict()
+                res.append(
+                    TodoData(date=d["date"], content=d["content"])
+                )
+            return res
+        except Exception as e:
+            self.logger.error(e)
+            raise TodoRegisterationError(
+                f"getting data from db is error! error detail is {e}"
+            )
 
 
 class TodoHandleError(Exception):
