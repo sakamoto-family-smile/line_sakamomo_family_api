4c659c8f74acba960efe3d5aad377293717c9688
sakamoto-family-smile <sakamoto-family-smile@gmail.com>
2024-08-11 23:54:34 +0900

    update codes for front

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -6,7 +6,7 @@ import json
 
 
 def load_firebase_auth():
-    with open(os.path.join("secret", "config.json")) as f:
+    with open(os.path.join(os.path.dirname(__file__), "secret", "config.json")) as f:
         firebase_config = json.load(f)
     app = pyrebase.pyrebase.initialize_app(config=firebase_config)
     return app.auth()
@@ -32,5 +32,20 @@ def app(auth):
 
 
 def main():
-    auth = load_firebase_auth()
-    app(auth=auth)
+    st.title("サンプルアプリ")
+    st.text("email: ")
+    email = st.text_input("email", "")
+    st.text("password: ")
+    password = st.text_input("password", type="password")
+
+    if st.button("login"):
+        if st.spinner("please wait for initialize.."):
+            auth = load_firebase_auth()
+        st.success("initialize is compeleted!")
+        if st.spinner("please wait for authenticate.."):
+            authenticate(auth=auth, email=email, password=password)
+        st.success("authenticate is completed!")
+
+
+if __name__ == "__main__":
+    main()
