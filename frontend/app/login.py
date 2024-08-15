bf5bfe58e73df377cb016db0c84ef9db9289675f
sakamoto-family-smile <sakamoto-family-smile@gmail.com>
2024-08-16 08:23:49 +0900

    update codes for front

--- frontend/app/login.py
+++ frontend/app/login.py
@@ -2,31 +2,30 @@ import streamlit as st
 import pyrebase.pyrebase
 import os
 import json
+import dataclasses
 
 
-# Identity Platform Func
-def load_firebase_auth():
-    with open(os.path.join(os.path.dirname(__file__), "secret", "config.json")) as f:
-        firebase_config = json.load(f)
-    app = pyrebase.pyrebase.initialize_app(config=firebase_config)
-    return app.auth()
+@dataclasses.dataclass
+class User:
+    email: str
+    id_token: str
 
 
-def sign_in_user(auth, email: str, password: str):
-    try:
-        user = auth.sign_in_with_email_and_password(email, password)
-        return user
-    except Exception as e:
-        raise IdentityPlatformException(f"Sign in Error is occured! Exception detail is {e}")
+class LoginHelper:
+    def __init__(self) -> None:
+        # Identity Platformの認証処理を行うためのインスタンスを取得
+        with open(os.path.join(os.path.dirname(__file__), "secret", "config.json")) as f:
+            firebase_config = json.load(f)
+        app = pyrebase.pyrebase.initialize_app(config=firebase_config)
+        self.__auth = app.auth()
+
+    def login(self, email: str, password: str) -> User:
+        try:
+            user = self.__auth.sign_in_with_email_and_password(email, password)
+            return User(email=email, id_token=user["idToken"])
+        except Exception as e:
+            raise IdentityPlatformException(f"Sign in Error is occured! Exception detail is {e}")
 
 
 class IdentityPlatformException(Exception):
     pass
-
-
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main()
---
+++ frontend/app/ui.py
@@ -0,0 +1,62 @@
+import streamlit as st
+from .login import LoginHelper
+from .backend_util import BackendRequester
+
+
+# グローバル変数
+TOKEN_KEY = "authenticated"
+
+
+def login_page(placeholder):
+    # ログインページ用のUIを作成
+    with placeholder.container():
+        st.text("email: ")
+        email = st.text_input("email", "")
+        st.text("password: ")
+        password = st.text_input("password", type="password")
+        login_button = st.button("Login")
+
+    # ログイン処理の実施
+    if login_button:
+        with st.spinner("please wait for initialize.."):
+            login_helper = LoginHelper()
+        st.success("initialize is compeleted!")
+
+        with st.spinner("please wait for login.."):
+            user = login_helper.login(email=email, password=password)
+            st.session_state[TOKEN_KEY] = user.id_token
+        st.success("login is completed!")
+
+    # トークンが取れていれば、ページをメインのページへ遷移する
+    if st.session_state[TOKEN_KEY]:
+        chat_page(placeholder=placeholder)
+
+
+def chat_page(placeholder):
+    backend_requester = BackendRequester()
+
+    # 一旦ダミーとして作成
+    with placeholder.container():
+        st.text("Main Page")
+        with st.spinner("please wait for health check.."):
+            backend_requester.request_health_check(token=st.session_state[TOKEN_KEY])
+        st.success("health check is completed!")
+
+
+
+def main():
+    st.set_page_config(page_title='Sakamomo-Family-App',
+                       page_icon=':chart_with_upwards_trend:',
+                       layout='wide',
+                       initial_sidebar_state="collapsed")
+    placeholder = st.empty()
+
+    # 認証済みの場合は、メインページへ
+    if st.session_state[TOKEN_KEY]:
+        chat_page(placeholder=placeholder)
+    else:
+        login_page(placeholder=placeholder)
+
+
+if __name__ == "__main__":
+    main()
