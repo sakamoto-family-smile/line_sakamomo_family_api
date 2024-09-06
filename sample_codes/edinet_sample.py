73f1be2004ef856526698eaf3d9e7f5ea8551662
sakamoto-family-smile <sakamoto-family-smile@gmail.com>
2024-09-07 00:35:36 +0900

    update sample codes of edinet

--- sample_codes/edinet_sample.py
+++ sample_codes/edinet_sample.py
@@ -1,17 +1,23 @@
 import requests
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 from copy import deepcopy
 import urllib.request
 import sys
 import os
+from argparse import ArgumentParser
+from enum import Enum
+from typing import Union
 
 
-## global parameters ##
-EDINET_API_KEY = "xxxx"
 
 
-class DownloadResult:
+class Mode(Enum):
+    DOWNLOAD_DOCUMENTS = 0
+    DOCUMENTS_LIST = 1
+
+
+class DownloadDocumentsResult:
     def __init__(self) -> None:
         self.__success_document_ids = []
         self.__error_document_ids = []
@@ -35,6 +41,31 @@ class DownloadResult:
         return deepcopy(self.__error_document_ids)
 
 
+class GetDocumentListResult:
+    def __init__(self) -> None:
+        self.df: Union[pd.DataFrame, None] = None
+        self.__success_dates = []
+        self.__error_dates = []
+
+    def get_success_counts(self) -> int:
+        return len(self.__success_dates)
+
+    def get_error_counts(self) -> int:
+        return len(self.__error_dates)
+
+    def append_success_date(self, d: datetime):
+        self.__success_dates.append(d)
+
+    def append_error_date(self, d: datetime):
+        self.__error_dates.append(d)
+
+    def get_success_dates(self) -> list:
+        return deepcopy(self.__success_dates)
+
+    def get_error_dates(self) -> list:
+        return deepcopy(self.__error_dates)
+
+
 def get_documents_info_dataframe(target_date: datetime) -> pd.DataFrame:
     url = 'https://disclosure.edinet-fsa.go.jp/api/v2/documents.json'
     params = {
@@ -43,7 +74,14 @@ def get_documents_info_dataframe(target_date: datetime) -> pd.DataFrame:
         "Subscription-Key": EDINET_API_KEY
     }
     response = requests.get(url, params=params)
+    if response.status_code != 200:
+        raise Exception(f"failed to get document list! http status code is {response.status_code}")
+
     json_data = response.json()
+    status_code = int(json_data["metadata"]["status"])
+    if status_code != 200:
+        raise Exception(f"failed to get document list! status code is {status_code}")
+
     documents = json_data['results']
     df = pd.DataFrame(documents)
     return df
@@ -51,7 +89,6 @@ def get_documents_info_dataframe(target_date: datetime) -> pd.DataFrame:
 
 def download_document(output_folder: str, doc_id: str):
     url = f'https://api.edinet-fsa.go.jp/api/v2/documents/{doc_id}'
-    print(doc['edinetCode'], doc['docID'], doc['filerName'], doc['docDescription'], doc['submitDateTime'], sep='\t')
     params = {
         "type": 2,
         "Subscription-Key": EDINET_API_KEY
@@ -72,17 +109,14 @@ def download_document(output_folder: str, doc_id: str):
             raise e
 
 
-if __name__ == "__main__":
-    print("--- start to script ---")
-
-    # EDINETから指定した日付の有価証券報告書のリストを取得する
-    output_folder = os.path.join(os.path.dirname(__file__), "output", datetime.now().strftime("%Y%m%d%H%M%S"))
-    os.makedirs(output_folder, exist_ok=True)
-    df = get_documents_info_dataframe(target_date=datetime.strptime("2024-05-17", "%Y-%m-%d"))
+def download_documents(output_folder: str, target_date: datetime) -> DownloadDocumentsResult:
+    df = get_documents_info_dataframe(target_date=target_date)
+    df.to_csv(os.path.join(output_folder, "documents.csv"))
 
     # doc_idから有価証券報告書をzip形式でダウンロードする
-    res = DownloadResult()
-    for index, doc in df.iterrows():
+    res = DownloadDocumentsResult()
+    for _, doc in df.iterrows():
+        print(doc['edinetCode'], doc['docID'], doc['filerName'], doc['docDescription'], doc['submitDateTime'], sep='\t')
         doc_id = doc['docID']
         try:
             download_document(output_folder=output_folder, doc_id=doc_id)
@@ -90,8 +124,68 @@ if __name__ == "__main__":
         except Exception as e:
             print(e)
             res.append_error_doc_id(doc_id=doc_id)
+    return res
 
-    # 取得結果を表示する
-    print("--- download results ---")
-    print(f"success count = {res.get_success_counts()}")
-    print(f"error count = {res.get_error_counts()}")
+
+def get_documents_list(duration_days: int) -> GetDocumentListResult:
+    current_date = datetime.now()
+    duration_days = int(args.duration_days)
+    dfs = []
+    res = GetDocumentListResult()
+    for day in range(duration_days):
+        target_date = current_date - timedelta(days=day)
+        print(target_date.strftime("%Y-%m-%d"))
+
+        try:
+            df = get_documents_info_dataframe(target_date=target_date)
+            dfs.append(df)
+            res.append_success_date(target_date)
+        except Exception as e:
+            print(f"failed to get document list. error detail is {e}.")
+            res.append_error_date(target_date)
+            continue
+    df = pd.concat(dfs, ignore_index=True)
+    res.df = df
+    return res
+
+
+if __name__ == "__main__":
+    print("--- start to script ---")
+
+    parser = ArgumentParser()
+    parser.add_argument("mode")
+    parser.add_argument("--target_date", type=str, help="ドキュメント一覧を取得する際の日付情報. YYYY-MM-DDの文字列で記載")
+    parser.add_argument("--duration_days", type=int, help="ドキュメント一覧情報を取得する際の期間を指定。最新日付から逆算した期間を日単位で指定")
+    args = parser.parse_args()
+    output_folder = os.path.join(os.path.dirname(__file__), "output", datetime.now().strftime("%Y%m%d%H%M%S"))
+    os.makedirs(output_folder, exist_ok=True)
+
+    mode = int(args.mode)
+    if mode == Mode.DOWNLOAD_DOCUMENTS.value:
+        # EDINETから指定した日付の有価証券報告書のリストを取得する
+        target_date_str = args.target_date
+        target_date = datetime.strptime(target_date_str, "%Y-%m-%d")
+        res = download_documents(output_folder=output_folder, target_date=target_date)
+
+        # 取得結果を表示する
+        print("--- document download results ---")
+        print(f"target date = {target_date_str}")
+        print(f"success count = {res.get_success_counts()}")
+        print(f"error count = {res.get_error_counts()}")
+
+    elif mode == Mode.DOCUMENTS_LIST.value:
+        # EDINETからドキュメント一覧を、本日から逆算して、指定した日数分取得する
+        # ドキュメントのpdfはダウンロードしない
+        duration_days = int(args.duration_days)
+        res = get_documents_list(duration_days=duration_days)
+        df = res.df
+        df.to_csv(os.path.join(output_folder, "documents.csv"), index=False)
+
+        # 取得結果を表示する
+        print("--- document list results ---")
+        print(f"duration days = {duration_days}")
+        print(f"document count is {len(df)}")
+        print("detail is the following..")
+        print(df)
+    else:
+        raise Exception(f"{args.mode} mode  is not implemented!")
