46f258bac018e9dfca24a36073ff9ca694e94d65
sakamoto-family-smile <sakamoto-family-smile@gmail.com>
2024-09-11 23:30:47 +0900

    imp sample codes

--- sample_codes/gemini_sample.py
+++ sample_codes/gemini_sample.py
@@ -1,8 +1,33 @@
+import os
+import google.generativeai as genai
+import PyPDF2
 
 
-def main():
-    pass
+API_KEY = "xxxx"
+
+
+def extract_text_from_pdf(pdf_path):
+    with open(pdf_path, 'rb') as pdf_file:
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        extracted_text = ""
+        for page in pdf_reader.pages:
+            text = page.extract_text()
+            if text:
+                extracted_text += text
+        return extracted_text
+
+
+def main(pdf_path: str):
+    genai.configure(api_key=API_KEY)
+    model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+    pdf_file = extract_text_from_pdf(pdf_path=pdf_path)
+    prompt = """
+    内容に含まれる財務三表の内容を踏まえて、企業の分析を行なってください。
+    """
+    res = model.generate_content([prompt, pdf_file])
+    print(res)
 
 
 if __name__ == "__main__":
-    main()
+    pdf_path = os.path.join(os.path.dirname(__file__), "sample_data", "S100T80B.pdf")
+    main(pdf_path=pdf_path)
